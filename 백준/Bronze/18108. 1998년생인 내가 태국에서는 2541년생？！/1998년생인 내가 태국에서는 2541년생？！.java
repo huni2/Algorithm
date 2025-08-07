c8ce6c4f3714c73d8072b2bc2a3d8720adaed317
huni2 <92710879+huni2@users.noreply.github.com>
2025-08-07 17:46:30 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+//		(1) 단어 개수(n)와 n개의 단어를 입력받아 String 배열에 저장한다.
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		System.out.println(n - 543);
+		
+		
+	}
+}
