a9cfaa8cf34af7be558a12eaf51ce8729707bd3a
huni2 <92710879+huni2@users.noreply.github.com>
2025-08-13 18:03:27 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 736 ms, Memory: 221852 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int j = 0; j < N; j++) {
+            st = new StringTokenizer(br.readLine()," ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
