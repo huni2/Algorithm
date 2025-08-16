ae3676ed0f3c2b9fa66399317301ea020a1b4411
huni2 <92710879+huni2@users.noreply.github.com>
2025-08-16 12:58:59 +0900

    [Bronze V] Title: A+B - 7, Time: 212 ms, Memory: 18616 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String args[]) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int a = in.nextInt();
- 
-		for (int i = 1; i <= a; i++) {
-			int c = in.nextInt();
-			int d = in.nextInt();
- 
-			System.out.println("Case #" + i + ": " + (c + d));
-		}
- 
-		in.close();
-	}
+    public static void main(String[] args)  {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i ++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            System.out.println("Case #"+ (i+1) + ": " + (a + b));
+        }
+    }
 }
