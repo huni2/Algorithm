cd2d41622169a2d8ec535d50d0fa77d9cff41bb1
huni2 <92710879+huni2@users.noreply.github.com>
2025-09-01 23:35:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        int x = System.in.read();
+        System.out.println(x);
+
+        }
+    }
