0c160887faa46968a72d24592ba2def10676c3d3
huni2 <92710879+huni2@users.noreply.github.com>
2025-09-15 00:54:18 +0900

    [Bronze I] Title: 약수들의 합, Time: 200 ms, Memory: 18868 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n == -1 || n == 0) 
+                break;
+
+            int[] arr = new int[n];
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0 && n != i) {
+                    arr[count] = i;
+                    count++;
+                    sum += i;
+                }
+            }
+
+            if (n == sum) {
+                System.out.print(n + " = ");
+                for (int i = 0; i < n; i++) {
+                    if (n / 2 == arr[i]) {
+                        System.out.println(arr[i]);
+                    } else if (arr[i] != 0) {
+                        System.out.print(arr[i] + " + ");
+                    }
+                }
+            } else if (n != sum) {
+                System.out.println(n + " is NOT perfect.");
+            }
+        }
+    }
+}
